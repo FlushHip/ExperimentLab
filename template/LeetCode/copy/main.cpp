cfdb1ab2bf9bb25535587de9e932a7cc9be80162
FlushHip <FlushHip.G@gmail.com>
2021-08-31 21:58:16 +0800

    🌈 style(leetcode-cn): 优化样例输出模板

--- template/LeetCode/copy/main.cpp
+++ template/LeetCode/copy/main.cpp
@@ -4,9 +4,15 @@
 
 int main(int argc, char **argv)
 {
+    const int EN = 2;
+
     auto sol = std::make_unique<Solution>();
 
-    std::cout << std::endl;
+    for (int L = 1; L <= EN; ++L) {
+        std::cout << "Example " << L << " :" << std::endl;
+
+        std::cout << std::endl << std::endl;
+    }
 
     return 0;
 }
