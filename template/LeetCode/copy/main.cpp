2ba3bcba5961b07b163d6ffece49630c8a8e752a
FlushHip <FlushHip.G@foxmail.com>
2021-09-07 12:17:30 +0800

    🌈 style(leetcode-cn): 修改模板，便于代码编写

--- template/LeetCode/copy/main.cpp
+++ template/LeetCode/copy/main.cpp
@@ -11,7 +11,8 @@ int main(int argc, char **argv)
     for (int L = 0; L < EN; ++L) {
         std::cout << "Example " << L + 1 << " :" << std::endl;
 
-        std::cout << std::endl << std::endl;
+        std::cout
+            << std::endl << std::endl;
     }
 
     return 0;
