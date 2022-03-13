b86cb46ffe375dcf6e4d01fbd6e377c46626627c
FlushHip <FlushHip.G@foxmail.com>
2022-03-13 14:24:33 +0800

    🌈 style(leetcode-cn): 实验一下新的输出模板

--- leetcode-cn/primeval/393/A/main.cpp
+++ leetcode-cn/primeval/393/A/main.cpp
@@ -11346,13 +11346,21 @@ int main(int argc, char **argv)
     };
 
     for (auto L = 0u; L < v.size(); ++L) {
-        std::cout << "\x1b[32mExample " << L + 1 << " :" << std::endl;
+        std::cout << "\x1b[32mExample " << L + 1 << " :\x1b[0m" << std::endl;
 
-        std::cout << "\t\x1b[33mdata-> \x1b[0m" << v[L] << std::endl;
+        std::cout << "  |-   \x1b[33mdata-> \x1b[0m" << v[L] << std::endl;
+
+        auto start_epoch = std::chrono::steady_clock::now();
 
         auto ans = sol->validUtf8(v[L]);
 
-        std::cout << "\t\x1b[31mans -> \x1b[0m" << std::boolalpha << ans
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>
+                            (std::chrono::steady_clock::now() - start_epoch);
+
+        std::cout << "  |-   \x1b[31mans -> \x1b[0m" << std::boolalpha << ans
+            << std::endl
+            << "    |- " << (duration > 1s ? "\x1b[31m" : "\x1b[36m")
+                << duration.count() << "ms\x1b[0m"
             << std::endl << std::endl;
     }
 
