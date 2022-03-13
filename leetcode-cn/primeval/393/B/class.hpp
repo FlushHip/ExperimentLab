ba664773af9dba13723ffa500f147660546b9faa
FlushHip <FlushHip.G@foxmail.com>
2022-03-13 12:13:30 +0800

    🌈 style(leetcode-cn): 代码中表示二进制
    C++14增加了二进制字面量

--- leetcode-cn/primeval/393/B/class.hpp
+++ leetcode-cn/primeval/393/B/class.hpp
@@ -3,19 +3,19 @@ public:
     bool validUtf8(vector<int>& data) {
         int n = (int)data.size();
         for (int i = 0; i < n; ) {
-            if ((data[i] >> 7) == 0x0) {
+            if ((data[i] >> 7) == 0b0) {
                 i += 1;
-            } else if ((data[i] >> 5) == 0x6
-                && (i + 1 < n && (data[i + 1] >> 6) == 0x2)) {
+            } else if ((data[i] >> 5) == 0b110
+                && (i + 1 < n && (data[i + 1] >> 6) == 0b10)) {
                 i += 2;
-            } else if ((data[i] >> 4) == 0xE
-                && (i + 1 < n && (data[i + 1] >> 6) == 0x2)
-                && (i + 2 < n && (data[i + 2] >> 6) == 0x2)) {
+            } else if ((data[i] >> 4) == 0b1110
+                && (i + 1 < n && (data[i + 1] >> 6) == 0b10)
+                && (i + 2 < n && (data[i + 2] >> 6) == 0b10)) {
                 i += 3;
-            } else if ((data[i] >> 3) == 0x1E
-                && (i + 1 < n && (data[i + 1] >> 6) == 0x2)
-                && (i + 2 < n && (data[i + 2] >> 6) == 0x2)
-                && (i + 3 < n && (data[i + 3] >> 6) == 0x2)) {
+            } else if ((data[i] >> 3) == 0b11110
+                && (i + 1 < n && (data[i + 1] >> 6) == 0b10)
+                && (i + 2 < n && (data[i + 2] >> 6) == 0b10)
+                && (i + 3 < n && (data[i + 3] >> 6) == 0b10)) {
                 i += 4;
             } else {
                 return false;
