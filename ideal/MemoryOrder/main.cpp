8724f5be888a9a6f3ec5a794aa6284a623159a49
FlushHip <flushhip.g@gmail.com>
2022-05-07 09:26:55 +0800

    ✨ feat(ideal): 简单学习C++的原子变量内存序

---
+++ ideal/MemoryOrder/main.cpp
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void exp1();
+void exp2();
+void exp3();
+void exp4();
+void exp5();
+
+int main(int argc, char **argv)
+{
+    // exp1();
+    // exp2();
+    // exp3();
+    // exp4();
+    exp5();
+    return 0;
+}
