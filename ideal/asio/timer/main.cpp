36a086fabdd1eb5693aeb330f257e50e7edf8d3b
FlushHip <FlushHip.G@foxmail.com>
2022-06-27 12:49:56 +0800

    ✨ feat(ideal): 加入spdlog

--- ideal/asio/timer/main.cpp
+++ ideal/asio/timer/main.cpp
@@ -1,21 +1,42 @@
 #include <iostream>
 
 #include <boost/asio.hpp>
-#include <boost/log/trivial.hpp>
+
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+
 #include <spdlog/spdlog.h>
 
 void synchronous()
 {
-    spdlog::info(__LINE__);
+    SPDLOG_DEBUG("start");
 
     boost::asio::io_context io;
-    boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
+    boost::asio::steady_timer t(io, boost::asio::chrono::seconds(2));
     t.wait();
 
-    spdlog::info(__LINE__);
+    SPDLOG_TRACE("end");
+}
+
+void asynchronous()
+{
+    SPDLOG_DEBUG("start");
+    boost::asio::io_context io;
+    boost::asio::steady_timer t(io, boost::asio::chrono::seconds(2));
+    t.async_wait([](const boost::system::error_code &)
+    {
+        SPDLOG_DEBUG("finish");
+    });
+
+    io.run();
+    SPDLOG_DEBUG("end");
 }
 
 int main(int argc, char *argv[]) {
+    spdlog::set_level(spdlog::level::trace);
+    spdlog::set_pattern("[%Y-%m-%d %T] [%t] [%^%l%$] [%s:%#][%!] %v");
+
     synchronous();
+    asynchronous();
+
     return 0;
 }
