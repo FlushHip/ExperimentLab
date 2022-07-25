9bcc39148375b6105bfbcb8eba73c1cfe95c646d
FlushHip <flushhip.g@gmail.com>
2022-07-25 18:13:29 +0800

    🐎 ci(usb_capture_pack): 完善打包脚本

--- pack.bat
+++ pack.bat
@@ -1,8 +1,17 @@
 cd %~dp0
-call "%VCINSTALLDIR%\Auxiliary\Build\vcvars64.bat"
+if not defined DevEnvDir (
+    call "%VCVARSALL_PATH%\vcvarsall.bat" x64
+)
+if %errorlevel% neq 0 (goto finish)
 
 cmake -DCMAKE_BUILD_TYPE:STRING=Debug -S. -Bbuild -G Ninja
 if %errorlevel% neq 0 (goto finish)
+
+cmake --build build --target clean
+if %errorlevel% neq 0 (goto finish)
+if exist "build/install" (rd /S /Q "build/install")
+if %errorlevel% neq 0 (goto finish)
+
 cmake --build build --config Debug --target usb_capture --
 if %errorlevel% neq 0 (goto finish)
 cmake --install build
