21793950b4282e510d734ce1f8577663c84164a8
FlushHip <flushhip.g@gmail.com>
2022-10-20 17:42:08 +0800

    ✨ feat(repos, hestina): 开发
    - 登录界面的头像显示
    - 账号密码框的初步校验

--- repos/hestina/client/i18n/lang_zh_CN.ts
+++ repos/hestina/client/i18n/lang_zh_CN.ts
@@ -4,22 +4,22 @@
 <context>
     <name>login_main_dlg</name>
     <message>
-        <location filename="../../../../.build/repos/hestina/client/ui_login_main_dlg.h" line="109"/>
+        <location filename="../../../../.build/repos/hestina/client/ui_login_main_dlg.h" line="115"/>
         <source>Input your account</source>
         <translation type="unfinished">请输入账号</translation>
     </message>
     <message>
-        <location filename="../../../../.build/repos/hestina/client/ui_login_main_dlg.h" line="110"/>
+        <location filename="../../../../.build/repos/hestina/client/ui_login_main_dlg.h" line="116"/>
         <source>Input your passward</source>
         <translation type="unfinished">请输入密码</translation>
     </message>
     <message>
-        <location filename="../../../../.build/repos/hestina/client/ui_login_main_dlg.h" line="111"/>
+        <location filename="../../../../.build/repos/hestina/client/ui_login_main_dlg.h" line="117"/>
         <source>Avatar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../.build/repos/hestina/client/ui_login_main_dlg.h" line="112"/>
+        <location filename="../../../../.build/repos/hestina/client/ui_login_main_dlg.h" line="118"/>
         <source>Login</source>
         <translation type="unfinished">登录</translation>
     </message>
--- repos/hestina/client/login_main_dlg.cpp
+++ repos/hestina/client/login_main_dlg.cpp
@@ -1,9 +1,17 @@
 #include "login_main_dlg.h"
+#include <qdebug.h>
+#include <qpushbutton.h>
+#include <qregexp.h>
+#include <qvalidator.h>
 
+#include <iostream>
 #include <memory>
 
+#include <QDebug>
 #include <QMouseEvent>
+#include <QRegExpValidator>
 
+#include "client/login_main_dlg.h"
 #include "ui_login_main_dlg.h"
 
 namespace hestina {
@@ -17,11 +25,30 @@ login_main_dlg::login_main_dlg(QWidget* parent)
     // setWindowFlags(Qt::Dialog | Qt::CustomizeWindowHint);
     setWindowFlag(Qt::FramelessWindowHint);
 
-    ui_->password->setEchoMode(QLineEdit::EchoMode::Password);
     ui_->logo_text->setPixmap(
         QPixmap(":/login/logo_text.png")
             .scaled(ui_->logo_text->size(), Qt::KeepAspectRatio,
                 Qt::SmoothTransformation));
+
+    ui_->avatar->resize(60, 60);
+    ui_->avatar->setPixmap(
+        QPixmap(":/avatar/default_avatar.png")
+            .scaled(ui_->avatar->size() - QSize{5, 5}, Qt::KeepAspectRatio,
+                Qt::SmoothTransformation));
+
+    ui_->password->setEchoMode(QLineEdit::EchoMode::Normal);
+    ui_->account->setValidator(
+        new QRegExpValidator(QRegExp("[1-9]\\d{6,10}"), this));
+
+    ui_->password->setEchoMode(QLineEdit::EchoMode::Password);
+    ui_->password->setValidator(
+        new QRegExpValidator(QRegExp("\\S{1,20}"), this));
+
+    connect(ui_->login_bnt, &QPushButton::clicked, [this]() {
+        qDebug() << "account : " << ui_->account->text()
+                 << "\npassward : " << ui_->password->text();
+        std::cerr << "clicked" << std::endl;
+    });
 }
 
 login_main_dlg::~login_main_dlg() = default;
