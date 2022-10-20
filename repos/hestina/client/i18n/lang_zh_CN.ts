4dbd9b704b0eb32d4f57cc57623834924dd27ab2
FlushHip <flushhip.g@gmail.com>
2022-10-20 16:09:59 +0800

    ✨ feat(repos, hestina): 国际化支持

---
+++ repos/hestina/client/i18n/lang_zh_CN.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>login_main_dlg</name>
+    <message>
+        <location filename="../../../../.build/repos/hestina/client/ui_login_main_dlg.h" line="109"/>
+        <source>Input your account</source>
+        <translation type="unfinished">请输入账号</translation>
+    </message>
+    <message>
+        <location filename="../../../../.build/repos/hestina/client/ui_login_main_dlg.h" line="110"/>
+        <source>Input your passward</source>
+        <translation type="unfinished">请输入密码</translation>
+    </message>
+    <message>
+        <location filename="../../../../.build/repos/hestina/client/ui_login_main_dlg.h" line="111"/>
+        <source>Avatar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../.build/repos/hestina/client/ui_login_main_dlg.h" line="112"/>
+        <source>Login</source>
+        <translation type="unfinished">登录</translation>
+    </message>
+</context>
+</TS>
--- repos/hestina/client/main.cpp
+++ repos/hestina/client/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <QApplication>
+#include <QTranslator>
 
 #include <lib/logger.hpp>
 
@@ -9,9 +10,13 @@
 int main(int argc, char* argv[]) {
     hestina::logger::init(spdlog::level::info);
 
-    LOG_INFO << "hestina-client";
+    LOG_INFO << "hestina-client, Local : " << QLocale().name().toStdString();
 
     QApplication app(argc, argv);
+    QTranslator translator;
+    translator.load("i18n/lang_zh_CN.qm");
+    QCoreApplication::installTranslator(&translator);
+
     hestina::client::ui::login_main_dlg login_main_dlg;
     login_main_dlg.show();
     return app.exec();
