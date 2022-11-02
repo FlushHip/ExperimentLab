c94886ff1283384db49983e191fa180d47fb2659
FlushHip <flushhip.g@gmail.com>
2022-11-02 09:32:08 +0800

    ✨ feat(leetcode-cn): 每日一题 - 1620
    https://leetcode.cn/problems/coordinate-with-maximum-network-quality/

---
+++ leetcode-cn/primeval/16/1620/main.cpp
@@ -0,0 +1,43 @@
+#include "headers.h"
+
+#include "class.hpp"
+
+int main(int argc, char** argv) {
+    auto sol = std::make_unique<Solution>();
+
+    VTList<std::vector<std::vector<int>>, int> v{
+        {
+            {{1, 2, 5}, {2, 1, 7}, {3, 1, 9}},
+            2,
+        },
+        {
+            {{23, 11, 21}},
+            9,
+        },
+        {
+            {{1, 2, 13}, {2, 1, 7}, {0, 1, 9}},
+            2,
+        },
+    };
+
+    for (auto L = 0U; L < v.size(); ++L) {
+        std::cout << "\x1b[32mExample " << L + 1 << " :\x1b[0m" << std::endl;
+
+        std::cout << "  |-   \x1b[33mdata-> \x1b[0m" << v[L] << std::endl;
+
+        auto start_epoch = std::chrono::steady_clock::now();
+
+        auto ans = sol->bestCoordinate(std::get<0>(v[L]), std::get<1>(v[L]));
+
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+            (std::chrono::steady_clock::now() - start_epoch) * 1000);
+
+        std::cout << "  |-   \x1b[31mans -> \x1b[0m" << std::boolalpha << ans
+                  << std::endl
+                  << "    |- " << (duration > 1s ? "\x1b[31m" : "\x1b[36m")
+                  << duration.count() << "ms\x1b[0m" << std::endl
+                  << std::endl;
+    }
+
+    return 0;
+}
