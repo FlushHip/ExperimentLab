e3127c68f270161eb4648cc43d5659b62b04c869
FlushHip <flushhip.g@gmail.com>
2022-12-20 11:06:00 +0800

    ✨ feat(leetcode-cn): 做题 - 1552
    最小值最大问题
    
    https://leetcode.cn/problems/magnetic-force-between-two-balls/

---
+++ leetcode-cn/primeval/15/1552/main.cpp
@@ -0,0 +1,35 @@
+#include "headers.h"
+
+#include "class.hpp"
+
+int main(int argc, char** argv) {
+    auto sol = std::make_unique<Solution>();
+
+    VTList<std::vector<int>, int> v{
+        {{1, 2, 3, 4, 7}, 3},
+        {{5, 4, 3, 2, 1, 1000000000}, 2},
+        {{79, 74, 57, 22}, 4},
+        {{1, 3, 5, 7}, 4},
+    };
+
+    for (auto L = 0U; L < v.size(); ++L) {
+        std::cout << "\x1b[32mExample " << L + 1 << " :\x1b[0m" << std::endl;
+
+        std::cout << "  |-   \x1b[33mdata-> \x1b[0m" << v[L] << std::endl;
+
+        auto start_epoch = std::chrono::steady_clock::now();
+
+        auto ans = sol->maxDistance(UNPACK_2(v[L]));
+
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+            (std::chrono::steady_clock::now() - start_epoch) * 1000);
+
+        std::cout << "  |-   \x1b[31mans -> \x1b[0m" << std::boolalpha << ans
+                  << std::endl
+                  << "    |- " << (duration > 1s ? "\x1b[31m" : "\x1b[36m")
+                  << duration.count() << "ms\x1b[0m" << std::endl
+                  << std::endl;
+    }
+
+    return 0;
+}
