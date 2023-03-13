47eb9928f0a51943a9a6506443795552d83c5b5d
FlushHip <flushhip.g@gmail.com>
2023-03-13 09:55:58 +0800

    ✨ feat(leetcode-cn): 删除链表中的节点 - 237
    https://leetcode.cn/problems/delete-node-in-a-linked-list/?favorite=ex0k24j

---
+++ leetcode-cn/primeval/02/237/main.cpp
@@ -0,0 +1,48 @@
+#include "headers.h"
+
+#include "class.hpp"
+
+int main(int argc, char** argv) {
+    auto sol = std::make_unique<Solution>();
+
+    ListNode* root = nullptr;
+    VTList<ListNode*, ListNode*> v{
+        {root = new ListNode(
+             4, new ListNode(5, new ListNode(1, new ListNode(9)))),
+            root->next},
+        {root = new ListNode(
+             4, new ListNode(5, new ListNode(1, new ListNode(9)))),
+            root->next->next},
+    };
+
+    auto print = [](ListNode* root) {
+        std::vector<int> ret;
+        for (; root != nullptr; root = root->next) {
+            ret.push_back(root->val);
+        }
+        return ret;
+    };
+
+    for (auto L = 0U; L < v.size(); ++L) {
+        std::cout << "\x1b[32mExample " << L + 1 << " :\x1b[0m" << std::endl;
+
+        std::cout << "  |-   \x1b[33mdata-> \x1b[0m" << v[L] << std::endl;
+
+        auto start_epoch = std::chrono::steady_clock::now();
+
+        auto ans = std::vector<std::vector<int>>{print(std::get<0>(v[L]))};
+        sol->deleteNode(std::get<1>(v[L]));
+        ans.push_back(print(std::get<0>(v[L])));
+
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+            (std::chrono::steady_clock::now() - start_epoch) * 1000);
+
+        std::cout << "  |-   \x1b[31mans -> \x1b[0m" << std::boolalpha << ans
+                  << std::endl
+                  << "    |- " << (duration > 1s ? "\x1b[31m" : "\x1b[36m")
+                  << duration.count() << "ms\x1b[0m" << std::endl
+                  << std::endl;
+    }
+
+    return 0;
+}
