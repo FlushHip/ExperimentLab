765e0a38803e6c04cf8e3ff23c2f5492dabe9f3b
FlushHip <flushhip.g@gmail.com>
2023-03-16 14:17:36 +0800

    ✨ feat(leetcode-cn): 环形链表 - 141
    https://leetcode.cn/problems/linked-list-cycle/

---
+++ leetcode-cn/primeval/01/141/class.hpp
@@ -0,0 +1,26 @@
+#include "headers.h"
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x, ListNode* n = nullptr) : val(x), next(n) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return false;
+        }
+        for (ListNode *p_1 = head, *p_2 = head->next; p_2 != nullptr;
+             p_1 = p_1->next, p_2 = p_2->next->next) {
+            if (p_1 == p_2) {
+                return true;
+            }
+            if (p_2->next == nullptr) {
+                break;
+            }
+        }
+        return false;
+    }
+};
