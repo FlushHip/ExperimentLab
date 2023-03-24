1146b19ee4da6bc1839569527a7e7ba7656195a5
FlushHip <flushhip.g@gmail.com>
2023-03-24 11:25:17 +0800

    ✨ feat(leetcode-cn): 把二叉搜索树转换为累加树 - 538
    1038
    
    https://leetcode.cn/problems/convert-bst-to-greater-tree/

---
+++ leetcode-cn/primeval/10/1038/class.hpp
@@ -0,0 +1,34 @@
+#include "headers.h"
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* bstToGst(TreeNode* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        int sum = 0;
+        convert(root, sum);
+        return root;
+    }
+
+private:
+    void convert(TreeNode* root, int& sum) {
+        if (root == nullptr) {
+            return;
+        }
+        convert(root->right, sum);
+        root->val += sum;
+        sum = root->val;
+        convert(root->left, sum);
+    }
+};
---
+++ leetcode-cn/primeval/10/1038/main.cpp
@@ -0,0 +1,74 @@
+#include "headers.h"
+
+#include "class.hpp"
+
+int main(int argc, char** argv) {
+    auto sol = std::make_unique<Solution>();
+
+    VList<TreeNode*> v{
+        new TreeNode(4,
+            new TreeNode(
+                1, new TreeNode(0), new TreeNode(2, nullptr, new TreeNode(3))),
+            new TreeNode(
+                6, new TreeNode(5), new TreeNode(7, nullptr, new TreeNode(8)))),
+        new TreeNode(0, nullptr, new TreeNode(1)),
+        new TreeNode(1, new TreeNode(0), new TreeNode(2)),
+        new TreeNode(
+            3, new TreeNode(2, new TreeNode(1), nullptr), new TreeNode(4)),
+    };
+
+    auto print = [](TreeNode* root) {
+        /*
+        std::stack<TreeNode*> st{{root}};
+        std::vector<int> ret;
+        auto* p = root;
+        while (p != nullptr || !st.empty()) {
+            if (p != nullptr) {
+                ret.push_back(p->val);
+                st.push(p);
+                p = p->left;
+            } else {
+                p = st.top();
+                st.pop();
+                p = p->right;
+            }
+        }
+        */
+        std::vector<int> ret;
+        std::queue<TreeNode*> que{{root}};
+        while (!que.empty()) {
+            auto* p = que.front();
+            que.pop();
+            ret.push_back(p->val);
+            if (p->left != nullptr) {
+                que.push(p->left);
+            }
+            if (p->right != nullptr) {
+                que.push(p->right);
+            }
+        }
+        return ret;
+    };
+
+    for (auto L = 0U; L < v.size(); ++L) {
+        std::cout << "\x1b[32mExample " << L + 1 << " :\x1b[0m" << std::endl;
+
+        std::cout << "  |-   \x1b[33mdata-> \x1b[0m" << print(v[L])
+                  << std::endl;
+
+        auto start_epoch = std::chrono::steady_clock::now();
+
+        auto ans = print(sol->bstToGst(v[L]));
+
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+            (std::chrono::steady_clock::now() - start_epoch) * 1000);
+
+        std::cout << "  |-   \x1b[31mans -> \x1b[0m" << std::boolalpha << ans
+                  << std::endl
+                  << "    |- " << (duration > 1s ? "\x1b[31m" : "\x1b[36m")
+                  << duration.count() << "ms\x1b[0m" << std::endl
+                  << std::endl;
+    }
+
+    return 0;
+}
