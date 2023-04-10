bf5fc2a2855e69d29c95e74c77c251be104f084a
FlushHip <flushhip.g@gmail.com>
2023-04-10 11:37:54 +0800

    ✨ feat(leetcode-cn): 移动零 - 283
    双指针，牺牲0的相对稳定性
    
    https://leetcode.cn/problems/move-zeroes/

---
+++ leetcode-cn/primeval/02/283/A/class.hpp
@@ -0,0 +1,9 @@
+#include "headers.h"
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        std::stable_partition(
+            nums.begin(), nums.end(), [](int num) { return num != 0; });
+    }
+};
