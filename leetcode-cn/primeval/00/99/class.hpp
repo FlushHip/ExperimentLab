a43ba265bb69d7b005a3848e7ebcfb9c8d9f69e1
FlushHip <flushhip.g@gmail.com>
2023-06-06 09:46:48 +0800

    ✨ feat(leetcode-cn): 恢复二叉搜索树 - 99
    Morris 中序遍历, 莫里斯。不用额外空间的中序遍历方法，精髓是利用叶子结点的冗余位置设置中序遍历的后驱。
    
    https://leetcode.cn/problems/recover-binary-search-tree/

---
+++ leetcode-cn/primeval/00/99/class.hpp
@@ -0,0 +1,57 @@
+#include "headers.h"
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void recoverTree(TreeNode* root) {
+        TreeNode *x = nullptr, *y = nullptr;
+        for (TreeNode* last = nullptr; root != nullptr;) {
+            if (root->left != nullptr) {
+                auto* left_r = root->left;
+                while (left_r->right != nullptr && left_r->right != root) {
+                    left_r = left_r->right;
+                }
+                if (left_r->right == nullptr) {
+                    left_r->right = root;
+                    root = root->left;
+                } else {
+                    left_r->right = nullptr;
+
+                    if (last != nullptr && root->val < last->val) {
+                        y = root;
+                        if (x == nullptr) {
+                            x = last;
+                        } else {
+                            break;
+                        }
+                    }
+                    // visit(root);
+                    last = root;
+                    root = root->right;
+                }
+            } else {
+                if (last != nullptr && root->val < last->val) {
+                    y = root;
+                    if (x == nullptr) {
+                        x = last;
+                    } else {
+                        break;
+                    }
+                }
+                // visit(root);
+                last = root;
+                root = root->right;
+            }
+        }
+        std::swap(x->val, y->val);
+    }
+};
---
+++ leetcode-cn/primeval/00/99/main.cpp
@@ -0,0 +1,50 @@
+#include "headers.h"
+
+#include "class.hpp"
+
+int main(int argc, char** argv) {
+    auto sol = std::make_unique<Solution>();
+
+    VList<TreeNode*> v{
+        new TreeNode(1, new TreeNode(3, nullptr, new TreeNode(2)), nullptr),
+        new TreeNode(
+            3, new TreeNode(1), new TreeNode(4, new TreeNode(2), nullptr)),
+    };
+
+    auto print = [](TreeNode* root) {
+        std::vector<int> ret;
+        std::function<void(TreeNode*, std::vector<int>&)> dfs =
+            [&dfs](TreeNode* root, std::vector<int>& ret) {
+                if (root == nullptr) {
+                    return;
+                }
+                dfs(root->left, ret);
+                ret.push_back(root->val);
+                dfs(root->right, ret);
+            };
+        dfs(root, ret);
+        return ret;
+    };
+
+    for (auto L = 0U; L < v.size(); ++L) {
+        std::cout << "\x1b[32mExample " << L + 1 << " :\x1b[0m" << std::endl;
+
+        std::cout << "  |-   \x1b[33mdata-> \x1b[0m" << print(v[L])
+                  << std::endl;
+
+        auto start_epoch = std::chrono::steady_clock::now();
+
+        sol->recoverTree(v[L]);
+
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+            (std::chrono::steady_clock::now() - start_epoch) * 1000);
+
+        std::cout << "  |-   \x1b[31mans -> \x1b[0m" << std::boolalpha
+                  << print(v[L]) << std::endl
+                  << "    |- " << (duration > 1s ? "\x1b[31m" : "\x1b[36m")
+                  << duration.count() << "ms\x1b[0m" << std::endl
+                  << std::endl;
+    }
+
+    return 0;
+}
