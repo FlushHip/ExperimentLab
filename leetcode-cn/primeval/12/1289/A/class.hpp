4963a3173d33d9b94737da2997763068cb6cd8a7
FlushHip <flushhip.g@gmail.com>
2023-08-10 17:40:10 +0800

    ✨ feat(leetcode-cn): 下降路径最小和 II - 1289
    https://leetcode.cn/problems/minimum-falling-path-sum-ii/description/

---
+++ leetcode-cn/primeval/12/1289/A/class.hpp
@@ -0,0 +1,21 @@
+#include "headers.h"
+
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        int n = static_cast<int>(grid.size());
+        auto dp = grid;
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                dp[i][j] = std::numeric_limits<int>::max();
+                for (int k = 0; k < n; ++k) {
+                    if (j != k) {
+                        dp[i][j] =
+                            std::min(dp[i][j], dp[i - 1][k] + grid[i][j]);
+                    }
+                }
+            }
+        }
+        return *std::min_element(dp[n - 1].begin(), dp[n - 1].end());
+    }
+};
