2fd8f5881625678a0326d6776bf3ac87debd24fd
FlushHip <flushhip.g@gmail.com>
2023-08-10 20:04:18 +0800

    🎈 perf(leetcode-cn): 时间复杂度优化到n^2

--- leetcode-cn/primeval/12/1289/B/class.hpp
+++ leetcode-cn/primeval/12/1289/B/class.hpp
@@ -4,19 +4,43 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n = static_cast<int>(grid.size());
+        if (n == 1) {
+            return grid[0][0];
+        }
         auto dp = grid;
-        // TODO(flushhip): optimize
+        std::priority_queue<std::pair<int, int>> last_maxs;
+        for (int i = 0; i < n; ++i) {
+            if (last_maxs.size() < 2) {
+                last_maxs.emplace(dp[0][i], i);
+            } else {
+                if (last_maxs.top().first > dp[0][i]) {
+                    last_maxs.pop();
+                    last_maxs.emplace(dp[0][i], i);
+                }
+            }
+        }
         for (int i = 1; i < n; ++i) {
+            auto back_min = last_maxs.top();
+            last_maxs.pop();
+            auto last_min = last_maxs.top();
+            last_maxs.pop();
             for (int j = 0; j < n; ++j) {
-                dp[i][j] = std::numeric_limits<int>::max();
-                for (int k = 0; k < n; ++k) {
-                    if (j != k) {
-                        dp[i][j] =
-                            std::min(dp[i][j], dp[i - 1][k] + grid[i][j]);
+                if (j != last_min.second) {
+                    dp[i][j] = grid[i][j] + last_min.first;
+                } else {
+                    dp[i][j] = grid[i][j] + back_min.first;
+                }
+
+                if (last_maxs.size() < 2) {
+                    last_maxs.emplace(dp[i][j], j);
+                } else {
+                    if (last_maxs.top().first > dp[i][j]) {
+                        last_maxs.pop();
+                        last_maxs.emplace(dp[i][j], j);
                     }
                 }
             }
         }
-        return *std::min_element(dp[n - 1].begin(), dp[n - 1].end());
+        return last_maxs.pop(), last_maxs.top().first;
     }
 };
